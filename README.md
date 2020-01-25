5580f01955f9747bcd53e6112a00c3db05208823
Prince Kumar <princebantikumar@gmail.com>
2020-01-25 15:50:07 +0530

    Script written to automate the cloning and installing the wifi driver

---
+++ README.md
@@ -0,0 +1,16 @@
+# Solution of Wifi Driver Not Found in HP Laptop Ubuntu
+The solution is taken from this link [wifi adapter not found](
+https://askubuntu.com/questions/1047245/wifi-adapter-not-found-in-ubuntu-18-04)
+
+#### Prerequisites:-
+* Working Internet Connection is required for cloning Git Repository
+* Git installed and configured configured
+
+#### Run the script
+* make the script executable  
+`chmod +x solution.sh`
+* to run by either by giving password in **solution.sh** file  
+`vi solution.sh`  
+ *change value of password there*
+* Or via command line arguments  
+`./solution.sh sudo_password`
---
+++ solution.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#replace with your own password or give it via command line
+password="password"
+if [[ "$1" -eq "" ]]
+then
+	password=$1
+fi
+
+cmd=$(echo $password | sudo -S lshw -C network -short 2>/dev/null | grep Wireless | awk '{print $2 " " $4}')
+if sudo -n true ; then
+	echo "I got the sudo password"
+else
+	echo "Wrong sudo password"
+	echo "Either replace the password in this script or give password via command line arguments"
+	echo "Exiting...."
+	exit 0
+fi
+
+#the return value is a single string having a space
+#splitting by space and storing in a array
+cmd=($cmd)
+echo "Wifi Interface "${cmd[0]}
+echo "Wifi Driver "${cmd[1]}
+if [ -d "rtlwifi_new" ]
+then
+	rm -rf rtlwifi_new
+fi
+git clone https://github.com/lwfinger/rtlwifi_new.git
+if [[ $? -ne 0 ]]; then
+	echo "Failed to cloned Repository"
+	echo "Exiting..."
+	exit 0
+fi
+exit 0
+cd rtlwifi_new
+make
+sudo make install
+sudo modprobe -rv ${cmd[1]}
+sudo modprobe -v ${cmd[1]} ant_sel=2
+sudo ip link set ${cmd[0]} up
+sudo iw dev ${cmd[0]} scan
+make clean
